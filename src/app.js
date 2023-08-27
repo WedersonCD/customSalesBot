cb490d1b2dcb27b9d82508e88d879844ad90e892
wederson <wedersoncabral@gmail.com>
2023-08-27 10:57:59 -0300

    Add serverConfig files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const  chatRoute = require('./routes/chatRoute');
+const serverConfig = require('./configs/serverConfig');
 
 app.use(express.json());
 app.use('/', chatRoute);
 
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = serverConfig.SERVER_HOST_NAME;
+const port      = serverConfig.SERVER_PORT;
 
 app.listen(port,'0.0.0.0', () => {
 
---
+++ src/configs/serverConfig.js
@@ -0,0 +1,8 @@
+const SERVER_PORT = '3000';
+const SERVER_HOST_NAME = '127.0.0.0';
+
+
+module.exports ={
+    SERVER_PORT,
+    SERVER_HOST_NAME
+}
