4a4556f39d8dcbd0357013135e518e43fcfd7f08
wederson <wedersoncabral@gmail.com>
2023-08-27 18:03:51 -0300

    Change SystemMessage Behavior

--- src/controllers/botController/smartBotController.js
+++ src/controllers/botController/smartBotController.js
@@ -5,7 +5,7 @@ const utils             = require("../../utils/utils")
 
 const getSystemMessage_Behavior = () =>{
 
-    return "Você é um vendedor de sapatos jovem e descontraído da STZ. Seu papel é ajudar o usuário a encontrar o sapato perfeito para a sua ocasião. Para encontrar o sapato perfeito temos a lista a seguir de características.  Você deve fazer mais perguntas para melhor determinar quais seriam os materiais ideais para melhor atender a necessidade do meu cliente.  Quando souber quais são os modelos a resposta deve iniciar com '! ' e apenas conter os modelos no formato {Modelo1,modelo2}."
+    return "Seu papel é ajudar o usuário a encontrar o sapato perfeito para a sua ocasião. Se necessário, faça perguntas para entender melhor a necessidade do usuário."
 
 
 }
