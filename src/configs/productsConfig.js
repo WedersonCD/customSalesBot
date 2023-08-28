fd9c2b478d213d9b9ecdd6017414f3c514acc17a
wederson <wedersoncabral@gmail.com>
2023-08-28 16:56:37 -0300

    create PRODUCTS_DISTINCT_ID_PREFIX propertie

--- src/configs/productsConfig.js
+++ src/configs/productsConfig.js
@@ -3,6 +3,6 @@ const productsConfig = {}
 productsConfig.PRODUCTS_FILE_NAME='products';
 productsConfig.PRODUCTS_COLUMN_ID='ean';
 productsConfig.PRODUCTS_COLUMN_COLOR='Cor';
-
+productsConfig.PRODUCTS_DISTINCT_ID_PREFIX='!PRD';
 
 module.exports =productsConfig
